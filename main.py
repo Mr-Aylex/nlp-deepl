da03fc4624b60e49b60b24f798bae59cdfda4232
alexandre <carmonealexandre@hotmail.fr>
2023-11-08 15:50:11 +0100

    add td files

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import os
-os.environ["KERAS_BACKEND"] = "torch"
 import torch
-import keras_core as keras
 
 # teste gpu with torch
 print(torch.cuda.is_available())
-# git change branch name
-# git branch -m <oldname> <newname>
